619f892c07c7ce285dd62f17bdf52a0f3e263bc2
asthraksha <146541057+asthraksha@users.noreply.github.com>
2026-07-20 14:36:17 +0530

    sort out deploy issue 1.0

---
+++ migrations/versions/4ce6ba032a65_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: 4ce6ba032a65
+Revises: 
+Create Date: 2026-07-20 14:35:40.088056
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4ce6ba032a65'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
